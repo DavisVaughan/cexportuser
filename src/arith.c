ea22dd20571c4b7a9d59eb61c3992a10443a6a02
DavisVaughan <davis@rstudio.com>
2019-07-02 17:55:49 -0400

    Add some notes about the requirements

--- src/arith.c
+++ src/arith.c
@@ -1,4 +1,13 @@
 #include "cexportuser.h"
+
+// In the DESCRIPTION:
+// Imports: cexport
+// LinkingTo: cexport
+// It is required that you Import the package! LinkingTo is not enough as you
+// have to fully load the package to be able to use its C callables
+
+// Now all you have to do is include the API file we created
+// and you can call `cexport_plus_one()`
 #include <cexport_api.h>
 
 SEXP cexportuser_plus_two(SEXP x) {
